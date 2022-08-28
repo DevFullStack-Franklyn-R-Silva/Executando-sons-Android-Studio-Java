5d16eeaa615d5e65837d4ec718b37d212ccbe867
Franklyn R <franklyn1998@live.com>
2022-08-28 16:48:53 -0300

    Executando sons

--- app/src/main/java/com/hadesfranklyn/mediaplayer/MainActivity.java
+++ app/src/main/java/com/hadesfranklyn/mediaplayer/MainActivity.java
@@ -2,13 +2,26 @@ package com.hadesfranklyn.mediaplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.teste);
+
+    }
+
+    public void executarSom(View view){
+        if(mediaPlayer != null){
+            mediaPlayer.start();
+        }
     }
 }
